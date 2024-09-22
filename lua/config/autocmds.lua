acc7c3e23d0570b2d0cb1160ac362a70b7ad8b72
zhenkai.sun <zhenkai.sun@qq.com>
2024-09-22 21:06:41 +0800

    add ClangFormat

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,3 @@
-vim.api.nvim_create_user_command('Ibd', 'bd | bn', {})
+vim.api.nvim_create_user_command("Ibd", "bd | bn", {})
+vim.api.nvim_create_user_command("NewClangFmtFile", "%!clang-format -style=Google -dump-config > .clang-format", {})
+vim.api.nvim_create_user_command("ClangFmt", "%!clang-format", {})
