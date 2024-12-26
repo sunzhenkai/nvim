828e1de333fee92bcddeacb54bcbd7820aa1518f
zhenkai.sun <zhenkai.sun@qq.com>
2024-12-26 14:39:07 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,13 +8,16 @@ vim.api.nvim_create_user_command("ClangFmt", "%!clang-format", {})
 
 -- custome config for neo-tree
 require("neo-tree").setup({
+	commands = {
+		go_to_parent_dir = function(state)
+			local node = state.tree:get_node()
+			require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+		end,
+	},
 	window = {
 		mappings = {
 			-- go to parent node
-			["IP"] = function(state)
-				local node = state.tree:get_node()
-				require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
-			end,
+			["IP"] = "go_to_parent_dir",
 		},
 	},
 })
