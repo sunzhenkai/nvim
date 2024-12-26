d0e7401ebb0c3b8bccf182acd610c4bba9dec473
zhenkai.sun <zhenkai.sun@qq.com>
2024-12-26 11:29:14 +0800

    add IP for neo-tree

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,20 @@
+-- custome command prefer starts with 'I'
+
+-- close current tab, and move to next one
 vim.api.nvim_create_user_command("Ibd", "bd | bn", {})
 vim.api.nvim_create_user_command("NewClangFmtFile", "%!clang-format -style=Google -dump-config > .clang-format", {})
+-- format current file using ClangFmt
 vim.api.nvim_create_user_command("ClangFmt", "%!clang-format", {})
+
+-- custome config for neo-tree
+require("neo-tree").setup({
+	window = {
+		mappings = {
+			-- go to parent node
+			["IP"] = function(state)
+				local node = state.tree:get_node()
+				require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+			end,
+		},
+	},
+})
