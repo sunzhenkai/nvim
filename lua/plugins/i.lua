e0b0ae1f8d830f895eac5cba2f5e6d5dd5e99247
zhenkai.sun <zhenkai.sun@qq.com>
2024-12-26 13:31:55 +0800

    update

--- lua/plugins/i.lua
+++ lua/plugins/i.lua
@@ -10,6 +10,13 @@ return {
 		},
 	},
 
+	-- change trouble config
+	{
+		"folke/trouble.nvim",
+		-- opts will be merged with the parent spec
+		opts = { use_diagnostic_signs = true },
+	},
+
 	-- tool
 	-- override nvim-cmp and add cmp-emoji
 	{
