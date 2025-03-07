87f8efe0264afe898f7e46f5952d10c1e8e7813d
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-07 11:48:34 +0800

    add markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,8 +1,10 @@
+-- install with yarn or npm
 return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
 	end,
+	ft = { "markdown" },
 }
