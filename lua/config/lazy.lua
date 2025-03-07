bd579dce7fe8fe096cd8809a45c2b7e001e22820
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-07 10:45:31 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,3 +53,39 @@ require("lazy").setup({
 		},
 	},
 })
+
+-- custome config for neo-tree
+require("neo-tree").setup({
+	commands = {
+		go_to_parent_dir = function(state)
+			local node = state.tree:get_node()
+			require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+		end,
+	},
+	window = {
+		mappings = {
+			-- go to parent node
+			["IP"] = "go_to_parent_dir",
+		},
+	},
+})
+
+-- mason
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+-- close current tab, and move to next one
+vim.api.nvim_create_user_command("Ibd", "bd | bn", {})
+vim.api.nvim_create_user_command("NewClangFmtFile", "%!clang-format -style=Google -dump-config > .clang-format", {})
+-- format current file using ClangFmt
+vim.api.nvim_create_user_command("ClangFmt", "%!clang-format --style=file", {})
+
+-- theme config
+-- vim.o.background = "light"
