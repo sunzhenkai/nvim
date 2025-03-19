6f8b61f6f0dc169965e84705f1bfeed67b8fc687
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-19 12:56:23 +0800

    update

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,9 @@
 return {
 	"L3MON4D3/LuaSnip",
-	build = vim.fn.has("win32") ~= 0 and "make install_jsregexp" or nil,
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 		"benfowler/telescope-luasnip.nvim",
