943a0c3b3945ab366801371e7daa74f10a5c5493
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-22 13:08:42 +0800

    add ai

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,35 @@
+return {
+	"olimorris/codecompanion.nvim",
+	-- enabled = false,
+	config = function()
+		require("codecompanion").setup({
+			adapters = {
+				deepseek = function()
+					-- get deepseek key from environment variable
+					local api_key = os.getenv("OPENAI_API_KEY")
+					if not api_key then
+						vim.notify("OPENAI_API_KEY not set！", vim.log.levels.ERROR)
+						return
+					end
+					return require("codecompanion.adapters").extend("deepseek", {
+						env = {
+							api_key = api_key,
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "deepseek",
+				},
+				inline = {
+					adapter = "deepseek",
+				},
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
