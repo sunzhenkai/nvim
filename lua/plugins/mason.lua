5dd672c114b8bae8aa86300831980f1c65b334d8
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-27 14:05:31 +0800

    add mason plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,25 +1,40 @@
 return {
-	"williamboman/mason.nvim",
-	opts = {
-		-- https://github.com/williamboman/mason-lspconfig.nvim
-		ensure_installed = {
-			"markdownlint-cli2",
-			"markdown-toc",
-			"cmakelang",
-			"cmakelint",
-			"clangd",
-			"gopls",
-			"rust-analyzer",
-			"clang-format",
-			"jsonlint",
-			"cmake-language-server",
-		},
-		ui = {
-			icons = {
-				package_installed = "✓",
-				package_pending = "➜",
-				package_uninstalled = "✗",
-			},
-		},
-	},
+  "williamboman/mason.nvim",
+  opts = {
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    ensure_installed = {
+      "markdownlint-cli2",
+      "markdown-toc",
+      "cmakelang",
+      "cmakelint",
+      "clangd",
+      "gopls",
+      "rust-analyzer",
+      "clang-format",
+      "jsonlint",
+      "cmake-language-server",
+      "gotestsum",
+      "golines",
+      "go-debug-adapter",
+      "golangci-lint",
+      "gotests",
+      "goimports",
+      "goimports-reviser",
+      "golangci-lint-langserver",
+      "lua-language-server",
+      "prettier",
+      "rust-analyzer",
+      "shfmt",
+      "stylua",
+      "sonarlint-language-server",
+      "gospel",
+    },
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  },
 }
